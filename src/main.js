843c06d17d5de6264f87ff82615c7dcc2c447ffc
Karlson Drendel <kdrendel99@gmail.com>
2021-03-27 21:47:18 -0700

    Fix indentation issues

--- src/main.js
+++ src/main.js
@@ -10,38 +10,38 @@ function clearField() {
 
 function displayConversion(dollars, apiCurrencyValue, currency) {
   let converted = (dollars * apiCurrencyValue).toFixed([2]);
-  $('.currency-conversion').text(`${dollars} is equal to ${converted} ${currency}'s`);
+  $('.currency-conversion').text(`$${dollars} is equal to ${converted} ${currency}'s`);
 }
 
 function displayErrors(error) {
   $('.show-errors').text(`${error}`);
 }
 
-function selectOther(){
+function selectOther() {
   $('.badSelection').text("This application is designed for only seven currencies. Please select one of the available for an exchange rate.");
 }
 
 $(document).ready(function() {
-  $('#submitButton').click(function(){
+  $('#submitButton').click(function() {
     const dollars = $('#dollars').val();
     let currency = $("#currency").val();
     clearField();
-    displayConversion(dollars,currency);
-    CurrencyConversion.getConversion(dollars,currency)
+    displayConversion(dollars, currency);
+    CurrencyConversion.getConversion(dollars, currency)
       .then(function(apiResponse) {
         if (apiResponse instanceof Error) {
           throw Error(`Conversion API error: ${apiResponse.message}`);
         }
-        if (currency === 'other'){
+        if (currency === 'other') {
           selectOther();
         }
         if (currency !== 'other') {
           let apiCurrencyValue = apiResponse.conversion_rates[currency];
-          displayConversion(dollars,apiCurrencyValue,currency);
-        } 
+          displayConversion(dollars, apiCurrencyValue, currency);
+        }
       })
-    
-      .catch(function(error){
+
+      .catch(function(error) {
         displayErrors(error.message);
       });
   });
